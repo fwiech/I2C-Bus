da58113f9e9eb262ee7036c9074396fbd3761d66
Florian Wiech <social.florian.wiech@gmail.com>
2018-01-27 22:14:50 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#define F_CPU 1000000UL
+
+#include <stdio.h>
+#include <string.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+int main() {
+
+  //…
+
+  return 0;
+}
