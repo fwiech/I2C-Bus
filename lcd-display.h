ef6bb9a27b1dd5f39c0358265a672f67d16225c5
Florian Wiech <social.florian.wiech@gmail.com>
2018-01-27 23:40:08 +0100

    Added lcd-display init

--- lcd-display.h
+++ lcd-display.h
@@ -14,4 +14,49 @@ void lcd_display_write(int data_4bit, int register_select) {
 	SEND_BYTE(data);
 }
 
+void lcd_display_init() {
+
+	// wait at least 15ms
+	DELAY_MS(15);
+
+	// interface set to 8-Bit
+	lcd_display_write(0b0011, 0);
+
+	// wait at least 4,1ms
+	DELAY_MS(5);
+
+	// interface set to 8-Bit
+	lcd_display_write(0b0011, 0);
+
+	// wait at least 110us
+	DELAY_MS(1);
+
+	// interface set to 8-Bit
+	lcd_display_write(0b0011, 0);
+
+	// interface set to 4-Bit
+	lcd_display_write(0b0010, 0);
+
+	// 2 doublesaced with 5x8 matrix
+	lcd_display_write(0b0010, 0);
+	lcd_display_write(0b1000, 0);
+
+	// display off
+	lcd_display_write(0b0000, 0);
+	lcd_display_write(0b1000, 0);
+
+	// delete display
+	lcd_display_write(0b0000, 0);
+	lcd_display_write(0b0001, 0);
+
+	// cursor to the right, no display shift
+	lcd_display_write(0b0000, 0);
+	lcd_display_write(0b0010, 0);
+
+	// display on
+	lcd_display_write(0b0000, 0);
+	lcd_display_write(0b1111, 0);
+
+}
+
 #endif
