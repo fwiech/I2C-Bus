df01d599b4488afac3074670a8a50e7b692cdf07
Florian Wiech <social.florian.wiech@gmail.com>
2018-01-27 22:50:36 +0100

    Added i2cbus Constants

--- i2cbus-transmitter.h
+++ i2cbus-transmitter.h
@@ -1,6 +1,32 @@
 #ifndef I2C_BUS_H
 #define I2C_BUS_H
 
+#ifndef SDA_HIGH_SCL_HIGH
+	#define SDA_HIGH_SCL_HIGH	SDA_HIGH; SCL_HIGH
+#endif
+
+#ifndef SDA_HIGH_SCL_LOW
+	#define SDA_HIGH_SCL_LOW	SDA_HIGH; SCL_LOW
+#endif
+
+#ifndef SDA_LOW_SCL_HIGH
+	#define SDA_LOW_SCL_HIGH	SDA_LOW; SCL_HIGH
+#endif
+
+#ifndef SDA_LOW_SCL_LOW
+	#define SDA_LOW_SCL_LOW		SDA_LOW; SCL_LOW
+#endif
+
+#ifndef DELAY
+	#ifndef _UTIL_DELAY_H_
+		#include <util/delay.h>
+	#endif
+	#ifndef DELAY_TIME
+		#define DELAY_TIME 200
+	#endif
+	#define DELAY _delay_us(DELAY_TIME)
+#endif
+
   //…
 
 #endif
--- main.c
+++ main.c
@@ -6,6 +6,18 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+
+#define SDA_HIGH    PORTB |= 0b00000010
+#define SDA_LOW     PORTB &= 0b11111101
+
+#define SCL_HIGH    PORTB |= 0b00000001
+#define SCL_LOW     PORTB &= 0b11111110
+
+#define PULLUP_ON   DDRB &=0b11111101
+#define PULLUP_OFF	DDRB |=0b00000011
+
+#define READ_SDA_VALUE  (PORTB & (1 << 1)) >> 1
+
 #include "i2cbus-transmitter.h"
 
 int main() {
