0d487522e22f7298b9d30c83a104be3654e7a8e3
Florian Wiech <social.florian.wiech@gmail.com>
2018-01-28 16:39:30 +0100

    Added library comment docs

--- i2cbus-transmitter.h
+++ i2cbus-transmitter.h
@@ -1,6 +1,29 @@
 #ifndef I2C_BUS_H
 #define I2C_BUS_H
 
+/*
+	github repo: <https://github.com/florianwiech/I2C-Bus>
+
+	library created by
+		> Florian Wiech		<https://github.com/florianwiech>
+		> Thomas Niestroj	<https://github.com/Manorka>
+
+	Requirements:
+	To use this library you have to define the following constants:
+
+	> SDA_HIGH		 // set SDA line to high
+	> SDA_LOW		   // set SDA line to low
+
+	> SCL_HIGH		 // set SCL line to high
+	> SCL_LOW		   // set SCL line to low
+
+	> PULLUP_ON		 // set SDA line to input
+	> PULLUP_OFF	 // set both SDA & SCL to output (important to set both!)
+
+	> DELAY			   // name of the delay function:
+	               // the DELAY constant is optional, if you want to use the <util/delay> library.
+*/
+
 #ifndef SDA_HIGH_SCL_HIGH
 	#define SDA_HIGH_SCL_HIGH	SDA_HIGH; SCL_HIGH
 #endif
--- lcd-display.h
+++ lcd-display.h
@@ -1,6 +1,28 @@
 #ifndef LCD_DISPLAY_H
 #define LCD_DISPLAY_H
 
+/*
+	github repo: <https://github.com/florianwiech/I2C-Bus>
+
+	library created by
+		> Florian Wiech		<https://github.com/florianwiech>
+		> Thomas Niestroj	<https://github.com/Manorka>
+
+	hardware components:
+		> Display: Type HD44780, 4-Bit-Mode
+      Specification: http://www.sprut.de/electronic/lcd/
+
+	Requirements:
+	To use this library you have to define the following constants:
+
+	> SEND_BYTE   // name of the send function:
+					      //		@param Integer address (8-Bit)
+					      //		@return Integer (1 -> ack, 0 -> transfer fails)
+
+	> DELAY_MS		// name of the delay function:
+					      // the DELAY constant is optional, if you want to use the <util/delay> library.
+*/
+
 
 #ifndef DELAY_MS
 	#ifndef _UTIL_DELAY_H_
