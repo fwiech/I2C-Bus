94ef33914cbffc5b4895b939cea484aed9d3f1ea
Florian Wiech <social.florian.wiech@gmail.com>
2018-01-30 13:04:32 +0100

    Split i2cbus to Header & Source File

---
+++ src/i2cbus-transmitter.c
@@ -0,0 +1,78 @@
+#include "i2cbus-transmitter.h"
+
+void i2cbus_init() {
+  PULLUP_OFF;
+}
+
+void i2cbus_send_start_condition() {
+	SDA_LOW_SCL_HIGH;
+	DELAY;
+	SDA_LOW_SCL_LOW;
+	DELAY;
+}
+
+void i2cbus_send_stop_condition() {
+	SDA_LOW_SCL_LOW;
+	DELAY;
+	SDA_LOW_SCL_HIGH;
+	DELAY;
+	SDA_HIGH_SCL_HIGH;
+}
+
+int i2cbus_data_write(int data) {
+	int index;
+	for(index = 7; index >= 0; index--) {
+        int thebit = (data & (1 << index)) >> index;
+		i2c_bus_create_beat(thebit);
+	}
+	return i2cbus_receive_ack();
+}
+
+int i2cbus_address_7_write(unsigned int address, int read_write) {
+	address = address | (read_write << 0);
+	return i2cbus_data_write(address);
+}
+
+/**
+ * ACK
+*/
+int i2cbus_receive_ack() {
+	int ack = 0;
+
+	PULLUP_ON;
+	DELAY;
+	SCL_HIGH;
+	DELAY;
+	if(READ_SDA_VALUE == 0) {
+		ack = 1;
+	}
+	DELAY;
+	SCL_LOW;
+	DELAY;
+	PULLUP_OFF;
+
+	return ack;
+}
+
+/**
+ * HELPER
+*/
+void i2c_bus_create_beat(int bit) {
+	if(bit == 1) {
+		SDA_HIGH_SCL_LOW;
+		DELAY;
+		SDA_HIGH_SCL_HIGH;
+		DELAY;
+		DELAY;
+		SDA_HIGH_SCL_LOW;
+		DELAY;
+	} else {
+		SDA_LOW_SCL_LOW;
+		DELAY;
+		SDA_LOW_SCL_HIGH;
+		DELAY;
+		DELAY;
+		SDA_LOW_SCL_LOW;
+		DELAY;
+	}
+}
--- src/i2cbus-transmitter.h
+++ src/i2cbus-transmitter.h
@@ -50,75 +50,13 @@
 	#define DELAY _delay_us(DELAY_TIME)
 #endif
 
-void i2cbus_init() {
-  PULLUP_OFF;
-}
+void i2cbus_init();
+void i2cbus_send_start_condition();
+void i2cbus_send_stop_condition();
+void i2c_bus_create_beat(int);
+int i2cbus_receive_ack();
+int i2cbus_data_write(int);
+int i2cbus_address_7_write(unsigned int, int);
 
-void i2cbus_send_start_condition() {
-	SDA_LOW_SCL_HIGH;
-	DELAY;
-	SDA_LOW_SCL_LOW;
-	DELAY;
-}
-
-void i2cbus_send_stop_condition() {
-	SDA_LOW_SCL_LOW;
-	DELAY;
-	SDA_LOW_SCL_HIGH;
-	DELAY;
-	SDA_HIGH_SCL_HIGH;
-}
-
-void i2c_bus_create_beat(int bit) {
-	if(bit == 1) {
-		SDA_HIGH_SCL_LOW;
-		DELAY;
-		SDA_HIGH_SCL_HIGH;
-		DELAY;
-		DELAY;
-		SDA_HIGH_SCL_LOW;
-		DELAY;
-	} else {
-		SDA_LOW_SCL_LOW;
-		DELAY;
-		SDA_LOW_SCL_HIGH;
-		DELAY;
-		DELAY;
-		SDA_LOW_SCL_LOW;
-		DELAY;
-	}
-}
-
-int i2cbus_receive_ack() {
-	int ack = 0;
-
-	PULLUP_ON;
-	DELAY;
-	SCL_HIGH;
-	DELAY;
-	if(READ_SDA_VALUE == 0) {
-		ack = 1;
-	}
-	DELAY;
-	SCL_LOW;
-	DELAY;
-	PULLUP_OFF;
-
-	return ack;
-}
-
-int i2cbus_data_write(int data) {
-	int index;
-	for(index = 7; index >= 0; index--) {
-        int thebit = (data & (1 << index)) >> index;
-		i2c_bus_create_beat(thebit);
-	}
-	return i2cbus_receive_ack();
-}
-
-int i2cbus_address_7_write(unsigned int address, int read_write) {
-	address = address | (read_write << 0);
-	return i2cbus_data_write(address);
-}
 
 #endif
